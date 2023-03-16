85d0bfc84a256fbad2258daf8fff9e6d0a391d4e
rajavijayakumar <86779889+rajavijayakumar-r@users.noreply.github.com>
2023-03-16 20:19:31 +0530

    made build published to verdaccio
    This is a beta build and should only be used for testing and should not be used for production

---
+++ package/dts/index.d.ts
@@ -0,0 +1,21 @@
+import { ReactElement, FC } from 'react';
+import { ColorValue } from 'react-native';
+
+interface BasicIconProps {
+    width?: string | number;
+    height?: string | number;
+    color?: ColorValue;
+    customUrl?: string;
+}
+interface YbCoreIconProps extends BasicIconProps {
+    readonly name?: string;
+    readonly forcedConversion?: boolean;
+    readonly disableFetching?: boolean;
+    readonly placeholder?: ReactElement;
+    readonly loadingPlaceholder?: ReactElement;
+    readonly invalidateCacheInDays?: number;
+}
+
+declare const YbCoreIcon: FC<YbCoreIconProps>;
+
+export { YbCoreIcon as default };
